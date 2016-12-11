73902623864b274e541f1e53e51ee170dcf7ff66
giuliom95 <giuliomartella1995@gmail.com>
2016-12-11 15:39:57 +0100

    Added a simple fps counter.

--- src/test.py
+++ src/test.py
@@ -19,19 +19,37 @@ def main():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     
-    time = 0
+    ticks = 0
+    old_ticks = ticks
+    old_time = pygame.time.get_ticks()
     
     world = Transform(
         None,
-        lambda x: (0, 0),
+        lambda x: (0.5*np.sin(x*(np.pi/64)), 0),
         lambda x: 0,
         lambda x: (1, 1))
     
     t1 = Transform(
         world,
-        lambda x: (0.5*np.sin(x*(np.pi/64)), 0.5*np.cos(x*(np.pi/64))),
-        lambda x: x*(np.pi/64),
+        lambda x: (0.6*np.cos(x*(np.pi/64)), 0.5*np.sin(x*(np.pi/64))),
+        lambda x: x*(np.pi/32),
         lambda x: (1, 1))
+    t2 = Transform(
+        world,
+        lambda x: (0.5*np.cos((x+32)*(np.pi/64)), 0.6*np.sin((x+32)*(np.pi/64))),
+        lambda x: x*(np.pi/32),
+        lambda x: (1, 1))
+    t3 = Transform(
+        world,
+        lambda x: (0.6*np.cos((x+64)*(np.pi/64)), 0.5*np.sin((x+64)*(np.pi/64))),
+        lambda x: x*(np.pi/32),
+        lambda x: (1, 1))
+    t4 = Transform(
+        world,
+        lambda x: (0.5*np.cos((x+96)*(np.pi/64)), 0.6*np.sin((x+96)*(np.pi/64))),
+        lambda x: x*(np.pi/32),
+        lambda x: (1, 1))
+    
     
     vertices = np.array([
         [-.25, -.25, 1],
@@ -41,6 +59,9 @@ def main():
     faces = [[0,1,2], [0,2,3]]
     colors = [[255,0,0]]*len(vertices)
     s1 = Shape(vertices, colors, faces, t1)
+    s2 = Shape(vertices, colors, faces, t2)
+    s3 = Shape(vertices, colors, faces, t3)
+    s4 = Shape(vertices, colors, faces, t4)
     
     while True:
         for event in pygame.event.get():
@@ -50,9 +71,12 @@ def main():
             
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         
-        time += 1
-        world.update(time)
-        t1.update(time)
+        world.update(ticks)
+        t1.update(ticks)
+        t2.update(ticks)
+        t3.update(ticks)
+        t4.update(ticks)
+        ticks += 1
         
         glBegin(GL_TRIANGLES)
         [
@@ -62,11 +86,38 @@ def main():
             ] 
             for face in s1.get_faces()
         ]
+        [
+            [
+                (glColor(color), glVertex(point)) 
+                for (color, point) in face
+            ] 
+            for face in s2.get_faces()
+        ]
+        [
+            [
+                (glColor(color), glVertex(point)) 
+                for (color, point) in face
+            ] 
+            for face in s3.get_faces()
+        ]
+        [
+            [
+                (glColor(color), glVertex(point)) 
+                for (color, point) in face
+            ] 
+            for face in s4.get_faces()
+        ]
         glEnd()
                 
         pygame.display.flip()
         
-        pygame.time.wait(10)
+        cur_time = pygame.time.get_ticks()
+        if cur_time - old_time > 1000:
+            print(ticks - old_ticks)
+            old_time = cur_time
+            old_ticks = ticks
+            
+        # pygame.time.wait(10)
 
 if __name__ == '__main__':
     main()
